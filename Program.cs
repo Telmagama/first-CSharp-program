02254b9f0cafa9f20ed51ea424008bd677c85550
Telma Gama <telmagamasilva@gmail.com>
2018-04-26 18:09:47 -0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8_Condicionais
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando o projeto 8 - Condicionais 2 ");
+
+            int idadeJoao = 18;
+            //int quantidadePessoas = 2;
+            //bool acompanhado = quantidadePessoas >= 2;
+            int idade = 68;
+            bool ehIdoso = idade >= 65;
+
+            Console.WriteLine(ehIdoso);
+
+            bool acompanhado = false;
+
+            if (idadeJoao >= 18 && acompanhado == true)
+            {
+                Console.WriteLine("Pode entrar.");
+            }
+            else
+            {
+                Console.WriteLine("não pode entrar.");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
