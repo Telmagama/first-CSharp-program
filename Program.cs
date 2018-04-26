385e8465feb438390c0c8c2302e9cd3ccc5de0e9
Telma Gama <telmagamasilva@gmail.com>
2018-04-26 12:48:51 -0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_CaracteresETextos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando o projeto 5 - caracteres e Textos");
+
+            char primeiraLetra = 'a';
+
+            Console.WriteLine(primeiraLetra);
+
+            primeiraLetra = (char)65;
+            Console.WriteLine(primeiraLetra);
+
+            primeiraLetra = (char)(primeiraLetra + 1);     // type casting
+            Console.WriteLine(primeiraLetra);
+
+            string titulo = "Alura Cursos de Tecnologia " + 2020;
+            string cursosProgramacao = 
+@"- .NET
+- JAVA 
+- JAVASCRIPT";
+
+            Console.WriteLine(titulo);
+            Console.WriteLine(cursosProgramacao);
+
+
+
+            Console.WriteLine("clique em enter para fechar!");
+
+            Console.ReadLine();
+
+        }
+    }
+}
